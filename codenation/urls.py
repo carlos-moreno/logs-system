1d0fe9278f1886b273df43543d77c708a6b04226
Carlos Moreno <omorenodovale@gmail.com>
2020-07-15 15:51:50 -0500

    feat(logs-system): Modifying urls structure

--- codenation/urls.py
+++ codenation/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from codenation.core.views import documentation_api
 
@@ -8,6 +7,5 @@ urlpatterns = [
     path('', documentation_api, name="documentation"),
     path('api/v1/', include('codenation.urls_api')),
     path('admin/', admin.site.urls),
-    path('get_token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
--- codenation/urls_api.py
+++ codenation/urls_api.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from codenation.core.views import AgentViewSet, EventViewSet
 from codenation.account.views import UserViewSet
+from codenation.core.views import AgentViewSet, EventViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
@@ -11,4 +12,6 @@ router.register('events', EventViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('get_token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
