015bfe914f06354c54add509d8e678d627798345
Carlos Moreno <omorenodovale@gmail.com>
2020-07-14 23:55:45 -0500

    feat(logs-system): Add users in API

--- codenation/urls.py
+++ codenation/urls.py
@@ -6,7 +6,7 @@ from codenation.core.views import documentation_api
 
 urlpatterns = [
     path('', documentation_api, name="documentation"),
-    path('api/v1/', include('codenation.core.urls')),
+    path('api/v1/', include('codenation.urls_api')),
     path('admin/', admin.site.urls),
     path('get_token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh_token/', TokenRefreshView.as_view(), name='token_refresh'),
--- codenation/urls_api.py
+++ codenation/urls_api.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from rest_framework import routers
 
 from codenation.core.views import AgentViewSet, EventViewSet
+from codenation.account.views import UserViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
 router.register('agents', AgentViewSet)
 router.register('events', EventViewSet)
 
